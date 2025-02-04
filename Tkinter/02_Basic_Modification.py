da26c7ebb183c3e7e827840115b75b4735d90e5b
Akshat Rawat <111777625+Akshat-Rwt@users.noreply.github.com>
2025-02-04 21:49:03 +0530

    Basic Modification

---
+++ Tkinter/02_Basic_Modification.py
@@ -0,0 +1,17 @@
+# Step 1 :  import tkinter
+
+from tkinter import *
+
+# Step 2 : gui interaction
+
+window  = Tk()
+
+# Step 3 : adding inputs
+
+window.title("Simple")
+window.geometry("500x700")  # Length x Breadth
+window.config(bg = "Yellow")
+
+
+# Step 4 : main loop
+window.mainloop()
